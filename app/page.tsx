6c94e0a661b09e9ab851a7d8e36430798ec14278
Maxim Litvinov <maximlitvinov.2611@gmail.com>
2023-07-02 14:52:44 +0300

    Add some structure and work with nav

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Nav } from "./src/components/Nav";
+
+export default function Home() {
+  return (
+    <>
+      <Nav />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    </>
+  );
+}
---
+++ app/src/components/Nav.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import Logo from "../../assets/img/nav-logo.svg";
+import home__icon from "../../assets/icons/home.svg";
+import about__icon from "../../assets/icons/about.svg";
+import contact_icon from "../../assets/icons/contact.svg";
+import testimonials__icon from "../../assets/icons/testimonials.svg";
+
+import style from "./navigation/Nav.module.css";
+
+export function Nav() {
+  const navigation: string[] = ["Home", "About", "Contact", "Testimonials"];
+  const icons: any = [
+    home__icon,
+    about__icon,
+    contact_icon,
+    testimonials__icon,
+  ];
+  return (
+    <nav>
+      <div className={style.container}>
+        <div className={style.logo__block}>
+          <Link href={"/"}>
+            <Image src={Logo} alt="Logo" width={70} />
+            <p className={style.logo__title}>Buhlercompany</p>
+            <p className={style.logo__text}>Buhlercompany is the People</p>
+          </Link>
+        </div>
+
+        <ul className="flex justify-end flex-wrap">
+          {navigation.map((el: string, index: number) => (
+            <li className={style.nav__list} key={index}>
+                <Link className={style.icon__block} href={`/${el.toLocaleLowerCase()}`}>
+                  <Image
+                    className="mx-2"
+                    src={icons[index]}
+                    alt={el}
+                    width={20}
+                  />
+                  {el}
+                </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+}
